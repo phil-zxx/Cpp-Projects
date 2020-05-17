927b5ef89caa624e5cd1ea803551c4d6c7b77e93
phil-zxx <32247590+phil-zxx@users.noreply.github.com>
2020-05-17 13:49:53 +0100

    Added script to extract TypeNames at compile type

---
+++ TypeNameAtCompileType/src/Script.cpp
@@ -0,0 +1,26 @@
+#include <TypeInfo.hpp>
+
+namespace MyOwn
+{
+    class TestClass { };
+}
+
+template<class T, class U>
+struct DerivedClass { };
+
+int main()
+{
+    static_assert(TypeInfo::getTypeName<int>()    == "int");
+    static_assert(TypeInfo::getTypeName<float>()  == "float");
+    static_assert(TypeInfo::getTypeName<double>() == "double");
+
+    #if defined(__clang__)
+        static_assert(TypeInfo::getTypeName<MyOwn::TestClass>()          == "MyOwn::TestClass");
+        static_assert(TypeInfo::getTypeName<DerivedClass<int, double>>() == "DerivedClass<int, double>");
+    #else
+        static_assert(TypeInfo::getTypeName<MyOwn::TestClass>()          == "class MyOwn::TestClass");
+        static_assert(TypeInfo::getTypeName<DerivedClass<int, double>>() == "struct DerivedClass<int,double>");
+    #endif
+
+    return 0;
+}
